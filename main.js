5e411af1bc6b4c5422950263b6d40df2f6ac6d06
Ariel Videla <ariel_videla@hotmail.com>
2022-10-23 18:10:37 -0300

    second commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.get('/productos', (request, response) => {
         let contenedor = ``;
         arrayProductos.map(
             item =>
-                (contenedor += `<div style="background-color: DarkSlateBlue; color: white; text-align: center; height: auto; width: 300px"><h2>Artista: ${item.nombre}</h2><img style="margin-bottom: 10px" height="250px" width="250px" src="${item.img}"><h3>Precio: $ ${item.precio}</h3></div>`)
+                (contenedor += `<div style="background-color: DarkSlateBlue; color: white; text-align: center; height: auto; width: 300px"><h2>Artista: ${item.nombre}</h2><h3>Album: ${item.album}</h3><h4>Año: ${item.año}</h4><img style="margin-bottom: 10px" height="250px" width="250px" src="${item.img}"><h3>Precio: $ ${item.precio}</h3></div>`)
         );
         response.send(
             `<h1 style="text-align: center">Todos los productos:</h1><section style="display: flex; justify-content: space-around">${contenedor}</section>`
@@ -33,7 +33,7 @@ app.get('/random', (request, response) => {
         const arrayProductos = await productos.getAll();
         let numero = Math.floor(Math.random() * arrayProductos.length);                                                                                                                                                                                                                                         
         const producto = await productos.getById(numero + 1);
-        let contenedor = `<div style="background-color: RebeccaPurple; color: white; text-align: center; height: auto; width: 300px"><h2>Artista: ${producto[0].nombre}</h2> <h3>Precio: $ ${producto[0].precio}</h3> <img style="margin-bottom: 10px"height="250px" src="${producto[0].img}"></div>`;
+        let contenedor = `<div style="background-color: RebeccaPurple; color: white; text-align: center; height: auto; width: 300px"><h2>Artista: ${producto[0].nombre}</h2><h3>Album: ${producto[0].album}</h3><h4>Año: ${producto[0].año}</h4><img style="margin-bottom: 10px"height="250px" src="${producto[0].img}"><h3>Precio: $ ${producto[0].precio}</h3></div>`;
         response.send(
             `<h1 style="text-align: center">Producto al azar:</h1><section style="display: flex; justify-content: space-around">${contenedor}</section>`
         );
