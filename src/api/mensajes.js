0abbcfd9704df8b9d324e61cbf1ffb6dc78f59eb
Ariel Videla <ariel_videla@hotmail.com>
2022-12-29 00:01:37 -0300

    Desafio doce

--- src/api/mensajes.js
+++ src/api/mensajes.js
@@ -1,6 +1,6 @@
 import options from '../connection/options.js'
 
-import ContenedorArchivo from '../contenedores/ContenedorArchivo.js'
+import ContenedorArchivo from '../contenedor/ContenedorArchivo.js'
 
 const mensajesApi = new ContenedorArchivo(`${options.fileSystem.path}/mensajes.json`)
 
--- src/api/productos.js
+++ src/api/productos.js
@@ -1,6 +1,6 @@
 import options from '../connection/options.js'
 
-import ContenedorArchivo from '../contenedores/ContenedorArchivo.js'
+import ContenedorArchivo from '../contenedor/ContenedorArchivo.js'
 
 const productosApi = new ContenedorArchivo(`${options.fileSystem.path}/productos.json`)
 
