6ebba1606d5270d87a13e0ebd516edfd1aaf8cbe
Ariel Videla <ariel_videla@hotmail.com>
2023-01-12 22:34:12 -0300

    Desafio trece

--- src/routers/web/home.js
+++ src/routers/web/home.js
@@ -1,22 +1,23 @@
 import { Router } from 'express'
+import { webAuth } from '../../auth/index.js'
+
+import path from 'path'
 
 const productosWebRouter = new Router()
 
-//Interceptar con middleware o con if, para restringir home si no hay sesion--------actualizado-*+++*------------------
-productosWebRouter.get('/home', (req, res) => {
-const nombre = req.session?.nombre
-    if (nombre) {
-              //Cambiar el nombre de usuario-------------------actualizado-*+++*------------------
-  
-        res.render(process.cwd() + '/views/pages/home.ejs',  { nombre: req.session.nombre })
-    } else {
-      res.sendFile(process.cwd() + '/views/login.html')
-    }
-    //-------------------------------------------------------*+++*------------------
+productosWebRouter.get('/', (req, res) => {
+    res.redirect('/home')
+})
+
+productosWebRouter.get('/home', webAuth, (req, res) => {
+    res.render(path.join(process.cwd(), '/views/pages/home.ejs'), {
+        username: req.user.username,
+        contador: req.user.contador
+    })
 })
 
 productosWebRouter.get('/productos-vista-test', (req, res) => {
-    res.sendFile(process.cwd() + '/views/productos-vista-test.html')
+    res.sendFile(path.join(process.cwd(), '/views/productos-vista-test.html'))
 })
 
 export default productosWebRouter
